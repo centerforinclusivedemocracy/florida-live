2a9bf3acdf6b537165745a40e13c5796caf6f3ba
ScottStetkiewicz <themuppetman66@yahoo.com>
2020-10-01 09:12:37 -0400

    pripoll

--- src/sitewide.js
+++ src/sitewide.js
@@ -452,7 +452,7 @@ DATA_PROFILES.fullmodel = {
         DATA_LAYERS.prc_black, DATA_LAYERS.prc_asian, DATA_LAYERS.prc_latino, DATA_LAYERS.prc_white,
     ],
     pointsofinterest: [
-        DATA_LAYERS.pricenter2020, DATA_LAYERS.poi_govish, DATA_LAYERS.poi_misc, DATA_LAYERS.poi,
+        DATA_LAYERS.pripoll2020, DATA_LAYERS.poi_govish, DATA_LAYERS.poi_misc, DATA_LAYERS.poi,
     ],
 };
 
